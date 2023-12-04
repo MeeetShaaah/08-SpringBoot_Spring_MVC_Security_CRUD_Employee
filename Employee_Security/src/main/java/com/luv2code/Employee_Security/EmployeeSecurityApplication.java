e6609d83e4725693000a8de3f6317b28835db526
Meet Shah <meetshah7199@gmail.com>
2023-12-04 16:21:56 -0500

    Start With S[ring MVC_Security_Employee

---
+++ Employee_Security/src/main/java/com/luv2code/Employee_Security/EmployeeSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.Employee_Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSecurityApplication.class, args);
+	}
+
+}
---
+++ Employee_Security/src/test/java/com/luv2code/Employee_Security/EmployeeSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.Employee_Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
